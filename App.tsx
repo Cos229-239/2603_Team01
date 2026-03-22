7404218815f242299b7fe6bbb6d3295ccfca43e4
ChristopherCecil <foxtrotpromo@gmail.com>
2026-03-22 13:14:39 -0800

    Added a JournalEntryScreen and navigation from the JournalScreen and AsyncStorage to the project so that the user can save Journal Entries locally and reload them as necessary.

--- App.tsx
+++ App.tsx
@@ -7,19 +7,29 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import JournalScreen from './src/screens/JournalScreen';
+import JournalEntryScreen from './src/screens/JournalEntryScreen';
 import MoodScreen from './src/screens/MoodScreen';
 import RubberDuckScreen from './src/screens/RubberDuckScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const JournalStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="JournalList" component={JournalScreen} options={{ title: 'My Journal' }} />
+      <Stack.Screen name="JournalEntry" component={JournalEntryScreen} options={{ title: 'New Entry' }} />
+    </Stack.Navigator>
+  );
+};
+
 const MainTabs = () => {
   return (
-    <Tab.Navigator screenOptions={{ tabBarActiveTintColor: '#007AFF', headerShown: true }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Journal" component={JournalScreen} />
-      <Tab.Screen name="Mood" component={MoodScreen} />
-      <Tab.Screen name="Rubber Duck" component={RubberDuckScreen} />
+    <Tab.Navigator screenOptions={{ tabBarActiveTintColor: '#007AFF', headerShown: false }}>
+      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: true }} />
+      <Tab.Screen name="Journal" component={JournalStack} />
+      <Tab.Screen name="Mood" component={MoodScreen} options={{ headerShown: true }} />
+      <Tab.Screen name="Rubber Duck" component={RubberDuckScreen} options={{ headerShown: true }} />
     </Tab.Navigator>
   );
 };
